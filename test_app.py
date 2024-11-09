c361aaad17e36c071a5d499b90df11cd936d7473
mehul025 <112761617+mehul025@users.noreply.github.com>
2024-11-09 13:44:08 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -15,5 +15,5 @@ def client(app):
 def test_home(client):
     response = client.get('/') 
     assert response.status_code == 200
-    expected_text = 'The CI-CD pipeline is been developed now....'
+    expected_text = 'The CI-CD pipeline been developed now......Horray'
     assert expected_text.encode() in response.data
