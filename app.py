6babf8966f859f9b3f16f8bb0f09133376cf0afe
mehul025 <mehulmahajan02@gmail.com>
2024-11-15 11:03:41 +0530

    updated app.py and test_app.py files

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def create_app():
 
     @app.route('/')
     def home():
-        return 'The CI-CD pipeline been developed now......Horray'
+        return 'The CI-CD pipeline been developed now......Horrayy'
 
     return app
 
